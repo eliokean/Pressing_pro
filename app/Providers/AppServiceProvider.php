cb98e01bd4a137ec50528d70ba59ecbbcf01e4d1
eliokean <yaoeliottkenan@gmail.com>
2026-04-03 04:17:18 +0000

    fix: force HTTPS scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,11 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
-
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         if (config('app.env') === 'production') {
-        URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
